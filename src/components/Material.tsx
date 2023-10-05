9b74062803ac6bd91b15d94feb74a99eab49423d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-05 12:42:26 +0300

    export client side material components

---
+++ src/components/Material.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {
+  Button,
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+
+export {
+  Button,
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+};
