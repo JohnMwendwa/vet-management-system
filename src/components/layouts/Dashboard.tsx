7e9ab0f891f7735a311ea79ce31085e44935811f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-08 15:14:08 +0300

    render the footer component

--- src/components/layouts/Dashboard.tsx
+++ src/components/layouts/Dashboard.tsx
@@ -4,16 +4,18 @@ import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import Configurator from "./Configurator";
 import ConfiguratorBtn from "./ConfiguratorBtn";
+import Footer from "./Footer";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="min-h-screen bg-blue-gray-50/50">
       <Sidebar />
-      <div className="p-4 xl:ml-80">
+      <div className="flex flex-col h-screen p-4 xl:ml-80">
         <Navbar />
         <Configurator />
         <ConfiguratorBtn />
-        {children}
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </div>
     </div>
   );
