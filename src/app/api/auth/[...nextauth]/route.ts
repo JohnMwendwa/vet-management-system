384f7b5b565188e75c712a8cbaca1cf85d116337
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-10 12:05:35 +0300

    create the login api route

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import connectDB from "@/database/connection";
+import User from "@/database/models/User";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      type: "credentials",
+      credentials: {
+        email: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        await connectDB();
+
+        const user = await User.findByCredentials(
+          credentials?.email!,
+          credentials?.password!
+        );
+
+        return {
+          email: user.email,
+          image: "",
+          name: `${user.firstName} ${user.lastName}`,
+          id: user._id.toString(),
+        };
+      },
+    }),
+  ],
+
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
