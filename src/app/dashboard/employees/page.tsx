53534f7b0c4febe17b809805ed08e64ec46cd859
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-12 01:27:32 +0300

    fetch employees data

--- src/app/dashboard/employees/page.tsx
+++ src/app/dashboard/employees/page.tsx
@@ -1,7 +1,19 @@
 import Employees from "@/components/dashboard/Employees";
+import connectDB from "@/database/connection";
+import User from "@/database/models/User";
 
-const EmployeesPage = () => {
-  return <Employees data={[]} />;
+const fetchEmployees = async () => {
+  await connectDB();
+  const data = await User.find({
+    $or: [{ role: "nurse" }, { role: "doctor" }],
+  });
+  const dataJSON = JSON.stringify(data);
+  return JSON.parse(dataJSON);
+};
+
+const EmployeesPage = async () => {
+  const employees = await fetchEmployees();
+  return <Employees data={employees} />;
 };
 
 export default EmployeesPage;
