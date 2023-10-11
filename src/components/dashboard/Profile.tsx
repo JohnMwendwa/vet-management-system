1f178cf4bff56f59786416d78d99fca7e9f08902
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-11 17:49:03 +0300

    create Profile component

---
+++ src/components/dashboard/Profile.tsx
@@ -0,0 +1,69 @@
+import {
+  Avatar,
+  Card,
+  CardBody,
+  Tab,
+  Tabs,
+  TabsHeader,
+  Typography,
+} from "@/components/Material";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  Cog6ToothIcon,
+  HomeIcon,
+} from "@heroicons/react/24/outline";
+
+const Profile = () => {
+  return (
+    <>
+      <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url(https://images.unsplash.com/photo-1531512073830-ba890ca4eba2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80)] bg-cover	bg-center">
+        <div className="absolute inset-0 h-full w-full bg-blue-500/50" />
+      </div>
+      <Card className="mx-3 -mt-16 mb-6 lg:mx-4">
+        <CardBody className="p-4">
+          <div className="mb-10 flex items-center justify-between gap-6">
+            <div className="flex items-center gap-6">
+              <Avatar
+                src=""
+                alt=""
+                size="xl"
+                className="rounded-lg shadow-lg shadow-blue-gray-500/40"
+              />
+              <div>
+                <Typography variant="h5" color="blue-gray" className="mb-1">
+                  John Mwendwa
+                </Typography>
+                <Typography
+                  variant="small"
+                  className="font-normal text-blue-gray-600"
+                >
+                  CEO / Co-Founder
+                </Typography>
+              </div>
+            </div>
+            <div className="w-96">
+              <Tabs value="app">
+                <TabsHeader>
+                  <Tab value="app">
+                    <HomeIcon className="-mt-1 mr-2 inline-block h-5 w-5" />
+                    App
+                  </Tab>
+                  <Tab value="message">
+                    <ChatBubbleLeftEllipsisIcon className="-mt-0.5 mr-2 inline-block h-5 w-5" />
+                    Message
+                  </Tab>
+                  <Tab value="settings">
+                    <Cog6ToothIcon className="-mt-1 mr-2 inline-block h-5 w-5" />
+                    Settings
+                  </Tab>
+                </TabsHeader>
+              </Tabs>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default Profile;
