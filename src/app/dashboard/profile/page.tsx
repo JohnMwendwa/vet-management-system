b629dc33f4dca022497f750e579b50b584e52590
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-13 13:43:05 +0300

    fetch user details

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,7 +1,19 @@
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Profile from "@/components/dashboard/Profile";
 
-const ProfilePage = () => {
-  return <Profile />;
+const fetchUserDetails = async () => {
+  const session = await getServerSession(authOptions);
+  const name = session?.user?.name!;
+  const email = session?.user?.email!;
+  return { name, email };
+};
+
+const ProfilePage = async () => {
+  const details = await fetchUserDetails();
+
+  return <Profile {...details} />;
 };
 
 export default ProfilePage;
