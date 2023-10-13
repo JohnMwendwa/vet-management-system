05feae0e0a9e3c1567dcc71507749f925851275e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-13 19:51:53 +0300

    render the waves svg

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,11 @@
 import SignupForm from "@/components/SignupForm";
+import WavesSvg from "@/components/WavesSvg";
 
 const SignupPage = () => {
   return (
-    <main className="h-screen w-full flex flex-col">
+    <main className="h-screen w-full flex flex-col bg-sky-300">
       <div className="w-full mx-auto flex justify-center items-center flex-1 px-4">
+        <WavesSvg />
         <SignupForm />
       </div>
     </main>
