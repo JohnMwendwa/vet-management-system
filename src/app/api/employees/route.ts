683422b6388deb838a5fac1a912d24683888e6b8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-14 19:04:26 +0300

    create api route for updating employee details

--- src/app/api/employees/route.ts
+++ src/app/api/employees/route.ts
@@ -97,3 +97,92 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    // check if user session exists
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "Not Allowed to Access this resource",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user?.email });
+    //   check if user who has active session exists
+    if (!user || user.role !== "doctor") {
+      return NextResponse.json(
+        {
+          error: "Not Allowed to Access this resource",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const { firstName, lastName, email, id, role } = await req.json();
+
+    // Validate user details before proceeding
+    if (
+      !firstName.trim() ||
+      !lastName.trim() ||
+      !email.trim() ||
+      !email.includes("@") ||
+      !role.trim()
+    ) {
+      return NextResponse.json(
+        {
+          error: "Please provide valid details",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const employee = await User.findById(id);
+
+    // check if employee whose details are being updated exists
+    if (!employee) {
+      return NextResponse.json(
+        {
+          error: "Something went wrong",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    // update employee details in the database
+    employee.firstName = firstName;
+    employee.lastName = lastName;
+    employee.email = email;
+    employee.role = role;
+    await employee.save();
+
+    return NextResponse.json(
+      {
+        message: "Employee details updated successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      {
+        error: (e as Error).message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
