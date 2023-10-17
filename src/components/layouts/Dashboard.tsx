26717f803eb720d4597723b9e454c39ba47d6e2c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-17 22:54:21 +0300

    fix navbar fixed not working on scroll

--- src/components/layouts/Dashboard.tsx
+++ src/components/layouts/Dashboard.tsx
@@ -10,7 +10,7 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="min-h-screen bg-blue-gray-50/50">
       <Sidebar />
-      <div className="flex flex-col h-screen p-4 xl:ml-80">
+      <div className="flex flex-col min-h-screen p-4 xl:ml-80">
         <Navbar />
         <Configurator />
         <ConfiguratorBtn />
