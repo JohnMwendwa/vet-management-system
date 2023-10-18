676ea28b173741a3f408bcded4ffe8694b9d3d8a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-18 14:23:11 +0300

    extend auth session to include custom callback values

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      id: string;
+      role: "doctor" | "nurse" | "client" | "admin";
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    user: {
+      role: "doctor" | "nurse" | "client" | "admin";
+    } & DefaultSession["jwt"];
+  }
+}
